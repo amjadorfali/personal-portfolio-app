574678803570ee7502f1fc9b8f648a1944cb0bf6
Amjad <amjadorfali3@gmail.com>
2022-12-19 20:28:05 +0300

    ⬆️ Upgrade deps minor versions

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,12 +1,4 @@
 <script lang="ts">
-	// import Drawer, { AppContent, Content } from '@smui/drawer';
-	// import List, { Item, Text } from '@smui/list';
-
-	// import { page } from '$app/stores';
-	// import MediaQuery from '$lib/MediaQuery.svelte';
-	// import { customSlide } from '$lib/initialLoader.svelte';
-	// import { cubicInOut } from 'svelte/easing';
-	// import { crossfade, fade } from 'svelte/transition';
 	import MobileMenu from './mobileMenu.svelte';
 	import DesktopMenu from './desktopMenu.svelte';
 </script>
