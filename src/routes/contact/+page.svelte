65ae77ceb331f082f31b1b2167d3224969ec6e44
Amjad <amjadorfali3@gmail.com>
2023-02-07 15:31:39 +0300

    💩 Tinkering with Netlify forms

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -36,6 +36,29 @@
 				</ul>
 			</div>
 		</div>
+
+		<form name="contact" on:submit|preventDefault method="POST" data-netlify="true">
+			<p>
+				<label>Your Name: <input type="text" name="name" /></label>
+			</p>
+			<p>
+				<label>Your Email: <input type="email" name="email" /></label>
+			</p>
+			<p>
+				<label
+					>Your Role: <select name="role[]" multiple>
+						<option value="leader">Leader</option>
+						<option value="follower">Follower</option>
+					</select></label
+				>
+			</p>
+			<p>
+				<label>Message: <textarea name="message" /></label>
+			</p>
+			<p>
+				<button type="submit">Send</button>
+			</p>
+		</form>
 	</section>
 </article>
 
