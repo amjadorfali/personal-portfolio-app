263c1929b9140907cffea0b0a7c8f429ba31145c
Amjad <amjadorfali3@gmail.com>
2024-08-21 22:48:40 +0300

    remove unused config

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import { imagetools } from 'vite-imagetools';
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit(), imagetools()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
 	server: {
 		open: true,
 		port: 3000
